bf526489080edad6996836c24981f094b7579591
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-11-06 00:40:06 -0300

    [NodeJS] Singleton pattern implementation for Categories

--- nodejs/02-rentService/src/modules/cars/repositories/CategoriesRepository.ts
+++ nodejs/02-rentService/src/modules/cars/repositories/CategoriesRepository.ts
@@ -7,10 +7,18 @@ import {
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
-  constructor() {
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
   }
 
+  public static getInstace(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+    return CategoriesRepository.INSTANCE;
+  }
   create({ name, description }: ICreateCategoryDTO): void {
     const category = new Category();
     Object.assign(category, {
--- nodejs/02-rentService/src/modules/cars/useCases/listCategories/index.ts
+++ nodejs/02-rentService/src/modules/cars/useCases/listCategories/index.ts
@@ -1,6 +1,8 @@
-import { categoriesRepository } from "../createCategory";
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 import { ListCategoriesController } from "./ListCategoriesController";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+const categoriesRepository = CategoriesRepository.getInstace();
 
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
 const listCategoriesController = new ListCategoriesController(
